f76b74395753a9f44f717de1daa4b184379b2478
YMChenLiye <ymchenliye@gmail.com>
2015-06-05 12:42:45 +0800

    2.55Completed

---
+++ CSAPP_Homework/Chapter2/show-bytes.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+typedef unsigned char *byte_pointer;
+
+void show_bytes(byte_pointer start, int len)
+{
+	for (int i = 0; i < len; i++)
+	{
+		printf(" %.2x", start[i]);
+		//%.2x indicates that an integer should be print in hexadecimal with at least two digits
+	}
+}
+
+void show_int(int x)
+{
+	show_bytes((byte_pointer)&x, sizeof(int));
+}
+
+void show_float(float x)
+{
+	show_bytes((byte_pointer)&x, sizeof(float));
+}
+
+void show_double(double x)
+{
+	show_bytes((byte_pointer)&x, sizeof(double));
+}
+
+void show_pointer(void *x)
+{
+	show_bytes((byte_pointer)&x, sizeof(void *));
+}
+
+
+int main()
+{
+	int x = 10;
+	show_int(x);
+	getchar();
+}
+
+
