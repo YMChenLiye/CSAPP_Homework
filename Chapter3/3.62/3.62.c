0ad20c27f6a60f09a7921699e089aa579570069d
YMChenLiye <ymchenliye@gmail.com>
2015-06-16 11:08:34 +0800

    3.63

---
+++ Chapter3/3.62/3.62.c
@@ -0,0 +1,63 @@
+﻿/*The following code transposes the elements of an M × M array, where M is a
+constant deﬁned by #define:
+
+void transpose(Marray_t A) {
+int i, j;
+for (i = 0; i < M; i++)
+for (j = 0; j < i; j++) {
+int t = A[i][j];
+A[i][j] = A[j][i];
+A[j][i] = t;
+}
+}
+
+When compiled with optimization level -O2, gcc generates the following code for
+the inner loop of the function:
+
+1 .L3:									lable:loop
+2 movl (%ebx), %eax						eax=(ebx)			ebx=&A[i][j]
+3 movl (%esi,%ecx,4), %edx				edx=(esi+4*ecx)		edx=&A[j][i]
+4 movl %eax, (%esi,%ecx,4)				(esi+4*ecx)=eax		(esi+4*ecx)=(ebx)
+5 addl $1, %ecx							ecx+=1				j++
+6 movl %edx, (%ebx)						(ebx)=edx	 -->		(ebx)=(esi+4*ecx)
+7 addl $52, %ebx						ebx+=52				ebx
+8 cmpl %edi, %ecx
+9 jl .L3								ecx < edi goto loop
+
+
+A. What is the value of M?			52/4=13
+B. What registers hold program values i and j?			i=edi		j=ecx
+C. Write a C code version of transpose that makes use of the optimizations
+that occur in this loop. Use the parameter M in your code rather than numeric
+constants.*/
+
+#define M 13
+typedef int Marray_t[M][M];
+void transpose(Marray_t A) {
+	int i, j;
+	for (i = 0; i < M; i++)
+		for (j = 0; j < i; j++) {
+			int t = A[i][j];
+			A[i][j] = A[j][i];
+			A[j][i] = t;
+		}
+}
+
+
+void transpose1(Marray_t A)
+{
+	int i, j;
+	for (i = 0; i < M; i++)
+	{
+		int *Ap1 = A[i];
+		int *Ap2 = A[0] + i;
+		for (j = 0; j < i; j++)
+		{
+			int t = *(Ap1 + j);
+			*(Ap1 + j) = *Ap2;
+			*Ap2 = t;
+			Ap2 += M;
+		}
+	}
+}
+
---
+++ Chapter3/3.63/3.63.c
@@ -0,0 +1,51 @@
+﻿/*Consider the following source code, where E1 and E2 are macro expressions de-
+clared with #define that compute the dimensions of array A in terms of parameter
+n. This code computes the sum of the elements of column j of the array.
+
+1 int sum_col(int n, int A[E1(n)][E2(n)], int j) {
+2 int i;
+3 int result = 0;
+4 for (i = 0; i < E1(n); i++)
+5 result += A[i][j];
+6 return result;
+7 }
+
+In compiling this program, gcc generates the following assembly code:
+n at %ebp+8, A at %ebp+12, j at %ebp+16
+
+1 movl 8(%ebp), %eax					eax=n
+2 leal (%eax,%eax), %edx				edx=2*n
+3 leal (%edx,%eax), %ecx				ecx=3*n
+4 movl %edx, %ebx						ebx=2*n
+5 leal 1(%edx), %eax					eax=1+2*n
+6 movl $0, %edx							edx=0
+7 testl %eax, %eax						test eax
+8 jle .L3								if (eax <=0) goto L3
+9 leal 0(,%ecx,4), %esi					esi=12*n
+10 movl 16(%ebp), %edx					edx=j
+11 movl 12(%ebp), %ecx					ecx=A
+12 leal (%ecx,%edx,4), %eax				eax=A+4*j
+13 movl $0, %edx						edx=0
+14 movl $1, %ecx						ecx=1
+15 addl $2, %ebx						ebx=2*n+2
+16 .L4:
+17 addl (%eax), %edx					edx+=(A+4*j)
+18 addl $1, %ecx						ecx++
+19 addl %esi, %eax						eax+=12*n
+20 cmpl %ebx, %ecx						ecx-ebx
+21 jne .L4
+22 .L3:
+23 movl %edx, %eax						eax=edx
+
+Use your reverse engineering skills to determine the deﬁnitions of E1 and E2.*/
+
+#define E1 2*n+2
+#define E2 3*n
+int sum_col(int n, int A[E1][E2], int j) {
+	int i;
+	int result = 0;
+	for (i = 0; i < E1; i++)
+		result += A[i][j];
+	return result;
+
+}
